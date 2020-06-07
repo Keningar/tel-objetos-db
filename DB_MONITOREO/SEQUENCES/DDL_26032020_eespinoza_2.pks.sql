5707e78fc2325eced5f859fc7bd426c5db789dbf
Erick Espinoza Torres <eespinoza@nextcode.ec>
2020-06-06 19:44:50 -0500

    [Sprint 3] TN : INT : Monitoreo : Nuevo : Implementación MS Monitoreo/Frontend

---
+++ DB_MONITOREO/SEQUENCES/DDL_26032020_eespinoza_2.pks.sql
@@ -0,0 +1,14 @@
+--------------------------------------------------------
+--  CREACIÓN DE SECUENCIAS DE LA TABLA ADMI_GRUPO
+--------------------------------------------------------
+CREATE SEQUENCE DB_MONITOREO.SEQ_ADMI_GRUPO             INCREMENT BY 1 MAXVALUE 9999999999999999999999999999 MINVALUE 1 NOCACHE;
+
+--------------------------------------------------------
+--  CREACIÓN DE SECUENCIAS DE LA TABLA INFO_GRUPO_DETALLE
+--------------------------------------------------------
+CREATE SEQUENCE DB_MONITOREO.SEQ_INFO_GRUPO_DETALLE       INCREMENT BY 1 MAXVALUE 9999999999999999999999999999 MINVALUE 1 NOCACHE;
+
+--------------------------------------------------------
+--  CREACIÓN DE SECUENCIAS DE LA TABLA INFO_GRUPO_USUARIO
+--------------------------------------------------------
+CREATE SEQUENCE DB_MONITOREO.SEQ_INFO_GRUPO_USUARIO        INCREMENT BY 1 MAXVALUE 9999999999999999999999999999 MINVALUE 1 NOCACHE;
